bde67c9d1e4de51a41bb5ad4d2a474091c3a35b8
Marcus <marlhan@proton.me>
2024-09-12 18:01:46 +0200

    C flake

---
+++ c.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          nativeBuildInputs = with pkgs; [
+            gcc
+            gdb
+            gnumake
+            binutils
+            valgrind
+            clang-tools
+          ];
+          buildInputs = with pkgs; [];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+      );
+}
